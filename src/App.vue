7b0593e64a4ff833261a9c19ef68e7330d8006c4
ZZZZihan <zihan4602@163.com>
2024-06-05 16:00:44 +0800

    fronted

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <RegisterForm v-if="!isLoggedIn && showRegister" @register-success="handleRegisterSuccess" />
+    <LoginForm v-if="!isLoggedIn && !showRegister" @login="handleLogin" />
+    <UserDashboard v-if="isLoggedIn" :token="token" />
+    <button v-if="!isLoggedIn" @click="toggleRegister">{{ showRegister ? 'Back to Login' : 'Register' }}</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from './components/LoginForm.vue';
+import RegisterForm from './components/RegisterForm.vue';
+import UserDashboard from './components/UserDashboard.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isLoggedIn: false,
+      token: null,
+      showRegister: false
+    };
+  },
+  methods: {
+    handleLogin(token) {
+      this.token = token;
+      this.isLoggedIn = true;
+      this.showRegister = false;
+    },
+    toggleRegister() {
+      this.showRegister = !this.showRegister;
+    },
+    handleRegisterSuccess() {
+      this.showRegister = false;
+      alert('Registration successful! Please login.');
+    }
+  },
   components: {
-    HelloWorld
+    LoginForm,
+    RegisterForm,
+    UserDashboard
   }
-}
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="register">
+      <h2>Register</h2>
+      <form @submit.prevent="register">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" v-model="username" id="username" required>
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" v-model="password" id="password" required>
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'RegisterForm',
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      async register() {
+        try {
+          await axios.post('http://localhost:3000/register', {
+            username: this.username,
+            password: this.password
+          });
+          alert('Registration successful');
+          this.$emit('registered');
+        } catch (error) {
+          alert('Error registering');
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .register {
+    max-width: 300px;
+    margin: 0 auto;
+    padding: 1em;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  </style>
+  
